d5c8dee454b3884d0ef0b628e07b3b0cd2e52c42
Jasmine Martin <jasminejeane@gmail.com>
2016-04-30 19:06:23 -0700

    added bootstrap features and ui routing

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider){
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'home.html',
+      controller: 'MainCtrl'
+    });
+
+    $urlRouterProvider.otherwise('home');
+}])
+
+.factory('posts', [function(){
+
+  var o ={
+    posts: []
+  };
+  return o;
+
+}])
 
 .controller('MainCtrl', [
 '$scope',
-function($scope){ 
+'posts',
+function($scope, posts){ 
 
 // $scope.test = 'Hello world!';
 
-$scope.posts = [
+$scope.posts = posts.posts;
 
-{title: 'post 1', upvotes: 5},
-{title: 'post 2', upvotes: 2},
-{title: 'post 3', upvotes: 15},
-{title: 'post 4', upvotes: 9},
-{title: 'post 5', upvotes: 4}
+// [
 
-];
+// {title: 'post 1', upvotes: 5},
+// {title: 'post 2', upvotes: 2},
+// {title: 'post 3', upvotes: 15},
+// {title: 'post 4', upvotes: 9},
+// {title: 'post 5', upvotes: 4}
+
+// ];
 
 $scope.addPost = function(){
 
